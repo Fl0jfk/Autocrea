b1badd0dd68ae58220bb260a71018be77e53777e
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-12-03 17:52:48 +0100

    fix header

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -31,9 +31,11 @@ function Header(){
             variants={{ visible: { y: 0 }, hidden: { y: "-100%" }}} 
             animate={hidden ? "hidden" : "visible"}
             transition={{duration: 0.35, ease: "easeInOut"}}
-            className={`flex p-4 justify-between w-full fixed z-[12] md:mb-[100px] h-[100px] ${opacityMenu} max-w-[1440px] self-center shadow-xl border-b bg-white`}>
-                <div className='w-2/12 flex items-center pt-4'>
-                    <Image src={LogoAutoCrea} alt='Logo Auto Crea' width={250} height={100}/>
+            className={`flex p-4 justify-between w-full fixed z-[12] md:mb-[100px] h-[100px] ${opacityMenu} self-center shadow-xl border-b bg-white`}>
+                <div className='w-2/12 flex items-center sm:w-full sm:ml-8'>
+                    <div className='w-[250px] h-[100px] flex items-center'>
+                        <Image src={LogoAutoCrea} alt='Logo Auto Crea' layout='responsive'/>
+                    </div>
                 </div>
                 <div className='w-10/12 flex justify-end items-center sm:mt-[-5px]'>
                     <Navbar menuOpened={menuOpened} onLinkClick={handleLinkClick}/>
